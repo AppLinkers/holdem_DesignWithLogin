89a706b99c9f3f32873ccf002724e591e14eca63
jsw7027 <jsw7027@gmail.com>
2021-09-01 15:33:16 +0900

    data

--- app/src/main/java/com/example/ticket/ui/pub/PubFragment.java
+++ app/src/main/java/com/example/ticket/ui/pub/PubFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,6 +30,8 @@ public class PubFragment extends Fragment {
         //지역 필터
         spinner = (Spinner) root.findViewById(R.id.sp_pub);
         tv_pub = (TextView) root.findViewById(R.id.tv_pub);
+
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -40,6 +43,9 @@ public class PubFragment extends Fragment {
 
             }
         });
+
+
+
         //리사이클러뷰
         recyclerView = root.findViewById(R.id.rcPub);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
@@ -56,13 +62,13 @@ public class PubFragment extends Fragment {
     //리사이클러뷰 데이터
     public void getData(){
         Pub h1 = new Pub( "Final Nine", "강남",
-                "1 데일리, 3장 몬스터, 1.5장 게임", "14:00");
+                "1 데일리, 3장 몬스터, 1.5장 게임", "14:00", "여기는 사랑이 넘치는 강남 파이널나");
         Pub h2 = new Pub( "Final Nine", "홍대",
-                "1장 데일리, 3장 몬스터, 1.5장 게임", "14:00");
+                "1장 데일리, 3장 몬스터, 1.5장 게임", "14:00","여기는 바가바가 넘치는 홍대 파이널나인 ");
         Pub h3 = new Pub( "Battle Play Pub", "홍대",
-                "1장 데일리", "14:00");
+                "1장 데일리", "14:00", "여기는 할 게임이 많은 배틀플레이 펍 ");
         Pub h4 = new Pub( "레인보우", "강남",
-                "10장 메인 하이롤러, 20장 사이", "14:00");
+                "10장 메인 하이롤러, 20장 사이", "14:00", "여기는 코로나가 이빠이인 레인보우 펍 ");
 
         adapter.addItem(h1);
         adapter.addItem(h2);
