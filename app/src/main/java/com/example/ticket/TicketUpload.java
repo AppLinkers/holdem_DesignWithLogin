f6dd24e91293a58e5b6acbe4898b938920a915d7
jsw7027 <jsw7027@gmail.com>
2021-09-26 17:01:22 +0900

    asd

---
+++ app/src/main/java/com/example/ticket/TicketUpload.java
@@ -0,0 +1,15 @@
+package com.example.ticket;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TicketUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_upload);
+
+    }
+}
