6ce8ee14ac0406c41a01a72a3f5620a2265603e0
jsw7027 <jsw7027@gmail.com>
2021-09-26 20:42:33 +0900

    asd

--- app/src/main/java/com/example/ticket/ui/dataService/Tickets.java
+++ app/src/main/java/com/example/ticket/ui/dataService/Tickets.java
@@ -3,11 +3,17 @@ package com.example.ticket.ui.dataService;
 import com.example.ticket.ui.entity.TicketDto;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface Tickets {
     @GET("/ticket/list")
     Call<List<TicketDto>> tickets();
+
+    @POST("ticket/new")
+    Call<TicketDto> addTicket(@Body Map<String, String> map);
 }
